bfa145c07554042063f68927cc7ee09f4676351a
HashirProjects <hashirrana2001@gmail.com>
2022-07-12 12:32:38 +0100

    fixed bugs in train model

--- trainModel.py
+++ trainModel.py
@@ -27,7 +27,6 @@ class createModel():
 
 			self.model.compile(optimizer=keras.optimizers.Adam(), loss='categorical_crossentropy', metrics=[keras.metrics.CategoricalAccuracy()])
 			#metrics will always be the same for classification problems so i didnt include them as parameters
-		self.model.summary()
 
 	def train(self, X, Y, validationSplit, batchsize, epochs):
 		tb = keras.callbacks.TensorBoard(log_dir = f"logs/{self.name}")
@@ -49,8 +48,8 @@ def modelforID(interval,iden,timesteps):
 
 	data = getData.reshapeData(data)
 
-	model = createModel(10,64,10,24,data.shape[1:],2,interval,iden,timesteps) # input shape must be samples,number of time steps in each sample,number of features in each timestep
-	model.train(values,labels,0.05,1,70)
+	model = createModel(10,32,data.shape[1:],2,interval,timesteps) # input shape must be samples,number of time steps in each sample,number of features in each timestep
+	model.train(data,labels,0.05,1,70)
 
 if __name__ == "__main__":
 	modelforID("5m",[449,4151],30)
